449dd68051dcb95797ba1c9eedf5056dc3bf5a9d
Rajesh23965 <rajeshky123456@gmail.com>
2024-04-04 23:47:24 +0545

    First commit

---
+++ index.js
@@ -0,0 +1,223 @@
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const countryList = {
+   AED: "AE",
+  AFN: "AF",
+  XCD: "AG",
+  ALL: "AL",
+  AMD: "AM",
+  ANG: "AN",
+  AOA: "AO",
+  AQD: "AQ",
+  ARS: "AR",
+  AUD: "AU",
+  AZN: "AZ",
+  BAM: "BA",
+  BBD: "BB",
+  BDT: "BD",
+  XOF: "BE",
+  BGN: "BG",
+  BHD: "BH",
+  BIF: "BI",
+  BMD: "BM",
+  BND: "BN",
+  BOB: "BO",
+  BRL: "BR",
+  BSD: "BS",
+  NOK: "BV",
+  BWP: "BW",
+  BYR: "BY",
+  BZD: "BZ",
+  CAD: "CA",
+  CDF: "CD",
+  XAF: "CF",
+  CHF: "CH",
+  CLP: "CL",
+  CNY: "CN",
+  COP: "CO",
+  CRC: "CR",
+  CUP: "CU",
+  CVE: "CV",
+  CYP: "CY",
+  CZK: "CZ",
+  DJF: "DJ",
+  DKK: "DK",
+  DOP: "DO",
+  DZD: "DZ",
+  ECS: "EC",
+  EEK: "EE",
+  EGP: "EG",
+  ETB: "ET",
+  EUR: "FR",
+  FJD: "FJ",
+  FKP: "FK",
+  GBP: "GB",
+  GEL: "GE",
+  GGP: "GG",
+  GHS: "GH",
+  GIP: "GI",
+  GMD: "GM",
+  GNF: "GN",
+  GTQ: "GT",
+  GYD: "GY",
+  HKD: "HK",
+  HNL: "HN",
+  HRK: "HR",
+  HTG: "HT",
+  HUF: "HU",
+  IDR: "ID",
+  ILS: "IL",
+  INR: "IN",
+  IQD: "IQ",
+  IRR: "IR",
+  ISK: "IS",
+  JMD: "JM",
+  JOD: "JO",
+  JPY: "JP",
+  KES: "KE",
+  KGS: "KG",
+  KHR: "KH",
+  KMF: "KM",
+  KPW: "KP",
+  KRW: "KR",
+  KWD: "KW",
+  KYD: "KY",
+  KZT: "KZ",
+  LAK: "LA",
+  LBP: "LB",
+  LKR: "LK",
+  LRD: "LR",
+  LSL: "LS",
+  LTL: "LT",
+  LVL: "LV",
+  LYD: "LY",
+  MAD: "MA",
+  MDL: "MD",
+  MGA: "MG",
+  MKD: "MK",
+  MMK: "MM",
+  MNT: "MN",
+  MOP: "MO",
+  MRO: "MR",
+  MTL: "MT",
+  MUR: "MU",
+  MVR: "MV",
+  MWK: "MW",
+  MXN: "MX",
+  MYR: "MY",
+  MZN: "MZ",
+  NAD: "NA",
+  XPF: "NC",
+  NGN: "NG",
+  NIO: "NI",
+  NPR: "NP",
+  NZD: "NZ",
+  OMR: "OM",
+  PAB: "PA",
+  PEN: "PE",
+  PGK: "PG",
+  PHP: "PH",
+  PKR: "PK",
+  PLN: "PL",
+  PYG: "PY",
+  QAR: "QA",
+  RON: "RO",
+  RSD: "RS",
+  RUB: "RU",
+  RWF: "RW",
+  SAR: "SA",
+  SBD: "SB",
+  SCR: "SC",
+  SDG: "SD",
+  SEK: "SE",
+  SGD: "SG",
+  SKK: "SK",
+  SLL: "SL",
+  SOS: "SO",
+  SRD: "SR",
+  STD: "ST",
+  SVC: "SV",
+  SYP: "SY",
+  SZL: "SZ",
+  THB: "TH",
+  TJS: "TJ",
+  TMT: "TM",
+  TND: "TN",
+  TOP: "TO",
+  TRY: "TR",
+  TTD: "TT",
+  TWD: "TW",
+  TZS: "TZ",
+  UAH: "UA",
+  UGX: "UG",
+  USD: "US",
+  UYU: "UY",
+  UZS: "UZ",
+  VEF: "VE",
+  VND: "VN",
+  VUV: "VU",
+  YER: "YE",
+  ZAR: "ZA",
+  ZMK: "ZM",
+  ZWD: "ZW",
+};
+
+$(document).ready(function() {
+    let dropdowns = $(".dropdown select");
+    let btn = $("#btn");
+
+    let fromCurr = $(".from select");
+    let toCurr = $(".to select");
+
+    let msg = $(".msg");
+
+    dropdowns.each(function() {
+        for (let currCode in countryList) {
+            let newOption = $("<option>");
+            newOption.text(currCode);
+            newOption.val(currCode);
+
+            if ($(this).attr("name") === "from" && currCode === "NPR") {
+                newOption.prop("selected", true);
+            } else if ($(this).attr("name") === "to" && currCode === "INR") {
+                newOption.prop("selected", true);
+            }
+
+            $(this).append(newOption);
+        }
+
+        $(this).on("change", function() {
+            updateFlag($(this));
+        });
+    });
+
+    const updateFlag = (element) => {
+        let currCode = element.val();
+        let countryCode = countryList[currCode];
+        let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+        let img = element.parent().find("img");
+        img.attr("src", newSrc);
+    };
+
+    btn.on("click", async function(evt) {
+        evt.preventDefault();
+        let amount = $(".amount input");
+        let amtVal = amount.val();
+
+        if (amtVal === "" || amtVal < 1) {
+            amtVal = 1;
+            amount.val("1");
+        }
+
+        const URL = `${BASE_URL}/${fromCurr.val().toLowerCase()}/${toCurr.val().toLowerCase()}.json`;
+        try {
+            let response = await fetch(URL);
+            let data = await response.json();
+            let rate = data[toCurr.val().toLowerCase()];
+            let finalAmount = amtVal * rate;
+            msg.text(`${amtVal} ${fromCurr.val()} = ${finalAmount} ${toCurr.val()}`);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+            msg.text("Error fetching data. Please try again later.");
+        }
+    });
+});
